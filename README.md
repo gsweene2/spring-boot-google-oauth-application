d088667cce41294a966ca6ea1ac4cfa088dba205
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2021-01-11 23:48:36 -0500

    google oauth login project

---
+++ README.md
@@ -0,0 +1,14 @@
+# spring-boot-google-oauth-application
+
+## Build
+
+```
+mvn clean install
+```
+
+
+## Run
+
+```
+mvn spring-boot:run
+```
---
+++ src/test/java/com/example/googleoauthapp/WebSecurityConfigTests.java
@@ -0,0 +1,33 @@
+package com.example.googleoauthapp;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WebSecurityConfigTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void accessUnsecuredResourceThenOk() throws Exception {
+		mockMvc.perform(get("/"))
+			.andExpect(status().isOk());
+	}
+
+	@Test
+	public void accessSecuredResourceUnauthenticatedThenRedirectsToLogin() throws Exception {
+		mockMvc.perform(get("/user"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrlPattern("**/login"));
+	}
+}
